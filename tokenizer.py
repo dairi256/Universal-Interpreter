043c61b31088e4d626d6b5d7dc3aff50abbaf929
dairi256 <72542198+dairi256@users.noreply.github.com>
2024-06-24 10:59:35 +0800

    Currently revamping the tokenizer, WIP.

--- tokenizer.py
+++ tokenizer.py
@@ -1,26 +1,9 @@
 import re 
 
-def tokenize(code):
-    tokens = []
-    keywords = r'\b(if|else|while|for|do|else)\b'
-    for match in re.finditer(keywords, code):
-        tokens.append(('KEYWORD', match.group()))
+LANGUAGE_MAP = {
 
-    identifiers = r'\b[a-zA-Z_][a-zA-Z_0-9]*\b'
-    for match in re.finditer(identifiers, code):
-        tokens.append(('IDENTIFIER', MATCH.GROUP()))
-
-    literals = r'(\d+|[+-]?\d*.\d+(?:[eE][+-]?\d+)?[+-?\d+[Ll]])'
-    for match in re.finditer(literals, code):
-        tokens.append(('IDENTIFIER', match.group()))
-
-    symbols = r'[<>]=|[<>]?=?|[+\-*/%&|^(){}[]:;.,]'
-    for match in re.finditer(symbols, code):
-        tokens.append(('SYMBOL', match.group()))
-
-    return tokens
-
-
-code = "x = 5 + 3 * 2"
-tokens = tokenize(code)
-print(tokens)
+    'python' : {
+        'keywords' : r'\b(if|else|while|for|do\def|class|try|except|finally)\b',
+        'identifiers' : r'\b[a-zA-Z_][a-zA-Z_0-9]*\b',
+        'literals' : r'(\d+|[+-]?\d*\.\d+(?:[eE][+-]?\d+)?|[+-]?\d+[Ll])',
+        'symbols' : r'[<>]=|[<>]?=?|'
