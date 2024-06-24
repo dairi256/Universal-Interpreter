0765456b531743c841d8856b3da4cefbe323858f
dairi256 <72542198+dairi256@users.noreply.github.com>
2024-06-24 14:13:40 +0800

    Second version starting point of the tokenizer, this might change in the future. THIS IS NOT FINAL.

--- tokenizer.py
+++ tokenizer.py
@@ -6,4 +6,35 @@ LANGUAGE_MAP = {
         'keywords' : r'\b(if|else|while|for|do\def|class|try|except|finally)\b',
         'identifiers' : r'\b[a-zA-Z_][a-zA-Z_0-9]*\b',
         'literals' : r'(\d+|[+-]?\d*\.\d+(?:[eE][+-]?\d+)?|[+-]?\d+[Ll])',
-        'symbols' : r'[<>]=|[<>]?=?|'
+        'symbols' : r'[<>]=|[<>]?=?|[+\-*/%&|^(){}[]:;.,]'
+    },
+    'java': {
+        'keywords' : r'\b(public|private|protected|static|abstract|final|synchronized|volatile)\b',
+        'identifiers' : r'\b[A-Za-z_][A-Za-z_0-9]*\b',
+        'literals' : r'(\d+|[+-]?\d*\.\\d+(?:[eE][+-]?\d+)?|[+-]?\d+[Ll])',
+        'symbols' : r'[<>]=|[<>]?=?|[+\-*/%&|^(){}[]:;.,]'
+
+    }
+}
+
+def tokenize(code, langage='python'):
+    tokens = []
+    language_info = LANGUAGE_MAP[language]
+    keywords = re.compile(language_info['keywords'])
+    identifiers = re.compile(language_info['identifiers'])
+    literals = re.compile(language_info['literals'])
+    symbols = re.compile(language_info['symbols'])
+
+    for match in re.finditer(keywords, code):
+        tokens.append(('KEYWORD', match.group()))
+
+    for match in re.finditer(identifiers, code):
+        tokens.append(('IDENTIFIER', match.group()))
+    else:
+        tokens.appned(('LITERAL', match.group()))
+
+    for match in re.finditer(symbols, code):
+        tokens.append(('SYMBOL', match.group()))
+
+        return tokens
+
