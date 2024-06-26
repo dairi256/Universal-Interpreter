2b9c9b2b032cd223884e3ec2737542728f999399
dairi256 <72542198+dairi256@users.noreply.github.com>
2024-06-26 09:41:59 +0800

    Basic implementation of the parser. will update functions, this is an incomplete version.

---
+++ parser.py
@@ -0,0 +1,40 @@
+class Parser:
+    def __init__(self, tokens):     
+        self.otkens = tokensself.current_token = 0
+
+    def parse(self):
+        ast = self.parse.statement()
+        if self.current_token < len(self.tokens) - 1:
+            raise SyntaxError("Unexpected token")
+        return ast
+
+    def parse_statement(self):
+        if self.current_token >= len(self.tokens):
+            raise SyntaxError("Unexpected end of input")
+        if self.tokens[self.current_token] == 'KEYWORD':
+            keyword = self.tokens[self.current_token]
+            self.current_token += 1
+            if keyword == 'if':
+                return self.parse_if_statement()
+            elif keyword == 'for':
+                return self.parse_for_loop()
+                # handle other keywords..
+        else:
+            raise SyntaxError("Expected keyword")
+    
+    def parse_if_statement(self):
+        # code for parse if statement
+        pass
+    
+    def parse_for_loop(self):
+        # code for pass for loop
+        pass
+    
+    # use this line or more lines to implement other parsing functions. 
+
+class ASTNode:
+    def __init__(self, token):
+        self.token = token
+
+        def __str__(self):
+            return str(self.token)
