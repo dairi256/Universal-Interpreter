d0bdb2b4271a14d7f8da554fbaae0f77765ef068
dairi256 <72542198+dairi256@users.noreply.github.com>
2024-06-26 11:39:37 +0800

    Corrected issues and made a revised version of the Parser class.

--- parser.py
+++ parser.py
@@ -1,9 +1,10 @@
 class Parser:
-    def __init__(self, tokens):     
-        self.otkens = tokensself.current_token = 0
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.current_token = 0
 
     def parse(self):
-        ast = self.parse.statement()
+        ast = self.parse_statement()
         if self.current_token < len(self.tokens) - 1:
             raise SyntaxError("Unexpected token")
         return ast
@@ -11,9 +12,12 @@ class Parser:
     def parse_statement(self):
         if self.current_token >= len(self.tokens):
             raise SyntaxError("Unexpected end of input")
-        if self.tokens[self.current_token] == 'KEYWORD':
-            keyword = self.tokens[self.current_token]
-            self.current_token += 1
+
+        token = self.tokens[self.current_token]
+        self.current_token += 1
+
+        if token == 'KEYWORD':
+            keyword = self.tokens[self.current_token - 1]
             if keyword == 'if':
                 return self.parse_if_statement()
             elif keyword == 'for':
@@ -21,20 +25,20 @@ class Parser:
                 # handle other keywords..
         else:
             raise SyntaxError("Expected keyword")
-    
+
     def parse_if_statement(self):
         # code for parse if statement
         pass
     
     def parse_for_loop(self):
-        # code for pass for loop
+        # code for parse for loop
         pass
     
-    # use this line or more lines to implement other parsing functions. 
+    # use this line or more lines to implement other parsing functions.
 
 class ASTNode:
     def __init__(self, token):
         self.token = token
 
-        def __str__(self):
-            return str(self.token)
+    def __str__(self):
+        return str(self.token)
